32ec56a6aebaf35aa9d473d9b8778d75d0ee7096
Ian Rauenzahn <ian@shroop.net>
2019-05-22 16:33:27 -0700

    Revert "trying to do sounds they maybe work?"
    This reverts commit 8a448bc7707bd7948560a91e29dd59c8e31461d8.

--- Project B Tatsu/src/other/Sounds.java
+++ Project B Tatsu/src/other/Sounds.java
@@ -8,7 +8,7 @@ import processing.core.PApplet;
 import processing.sound.*;
 
 public class Sounds {
-	private static Clip hadouken;
+	private static AudioInputStream hadouken;
 	private static Clip tatsu;
 	private static Clip srk;
 
@@ -18,37 +18,40 @@ public class Sounds {
 //		srk = new SoundFile(g, "srk2.wav");
 
 		try {
-			InputStream stream = Sounds.class.getResourceAsStream("resources/sounds/PL02_00013.wav");
-			AudioInputStream ais = AudioSystem.getAudioInputStream(stream);
-			hadouken = AudioSystem.getClip();
-			hadouken.open(ais);
-			
-			stream = Sounds.class.getResourceAsStream("resources/sounds/tatsu.wav");
-			ais = AudioSystem.getAudioInputStream(stream);
-			tatsu = AudioSystem.getClip();
-			tatsu.open(ais);
-			
-			stream = Sounds.class.getResourceAsStream("resources/sounds/srk2.wav");
-			ais = AudioSystem.getAudioInputStream(stream);
-			srk = AudioSystem.getClip();
-			srk.open(ais);
+
+//			tatsu = AudioSystem.getClip();
+//			stream = Sounds.class.getResourceAsStream("/resources/sounds/tatsu.wav");
+//			System.out.println(stream);
+//			inputStream = AudioSystem.getAudioInputStream(stream);
+//			tatsu.open(inputStream);
+//			
+//			srk = AudioSystem.getClip();
+//			inputStream = AudioSystem.getAudioInputStream(Main.class.getResourceAsStream("data/srk2.wav"));
+//			srk.open(inputStream);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	public static void hadouken() {
-		hadouken.setFramePosition(0);
-		hadouken.start();
+		try {
+			InputStream stream = Sounds.class.getResourceAsStream("/resources/sounds/PL02_00013.wav");
+			System.out.println(stream);
+			hadouken = AudioSystem.getAudioInputStream(stream);
+			Clip clip = AudioSystem.getClip();
+			hadouken.reset();
+			clip.open(hadouken);
+			clip.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	public static void tatsu() {
-		tatsu.setFramePosition(0);
-		tatsu.start();
-	}
+//	public static void tatsu() {
+//		tatsu.start();
+//	}
 
-	public static void srk() {
-		srk.setFramePosition(0);
-		srk.start();
-	}
+//	public static void srk() {
+//		srk.start();
+//	}
 }
